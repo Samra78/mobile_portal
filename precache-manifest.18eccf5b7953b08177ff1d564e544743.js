f0a2f875459f47c7c42bb55e1da10ade048155cb
Samra78 <samra.k@hcl.com>
2021-04-20 23:58:05 +0530

    Updates

---
+++ precache-manifest.18eccf5b7953b08177ff1d564e544743.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a97f74ad880dd99ff619",
+    "url": "/mobile_portal/static/css/main.1e3fb3e7.chunk.css"
+  },
+  {
+    "revision": "a97f74ad880dd99ff619",
+    "url": "/mobile_portal/static/js/main.a97f74ad.chunk.js"
+  },
+  {
+    "revision": "263c7d0fa5108ee498ab",
+    "url": "/mobile_portal/static/js/runtime~main.263c7d0f.js"
+  },
+  {
+    "revision": "4ba40f2639fc8fc2e019",
+    "url": "/mobile_portal/static/js/2.4ba40f26.chunk.js"
+  },
+  {
+    "revision": "284a3abd909c7a067480d2d4ad6c9936",
+    "url": "/mobile_portal/index.html"
+  }
+];
